c9ffc03184534aeb747d552d86cf91a2fef504f1
CBTT1 <714039364@qq.com>
2023-07-10 11:03:38 +0800

    test priority

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -1,5 +1,4 @@
-/*
- * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+/* SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: CC0-1.0
  */
@@ -14,33 +13,34 @@
 
 void myTask1(void *pvParam) {
   while (1) {
-    // printf("task1-111\n");
+    printf("task1-111\n");
     vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
 }
 
 void myTask2(void *pvParam) {
   while (1) {
-    // printf("task2-222\n");
+    printf("task2-222\n");
     vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
 }
 
 void myTask3(void *pvParam) {
   while (1) {
-    // printf("task3-333\n");
+    printf("task3-333\n");
     vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
 }
 /****main****/
 void app_main(void) {
-  UBaseType_t iPriority[3] = {0};
-  TaskHandle_t pxTask[3] = {NULL, NULL, NULL};
-  xTaskCreate(myTask1, "myTask1", 1024, NULL, 1, pxTask);
-  xTaskCreate(myTask2, "myTask2", 1024, NULL, 2, pxTask + 1);
-  xTaskCreate(myTask3, "myTask3", 1024, NULL, 3, pxTask + 2);
-  for (int i = 0; i < 3; i++) {
-    iPriority[i] = uxTaskPriorityGet(pxTask[i]);
-    printf("iPriority[%d] = %d\n", i, iPriority[i]);
-  }
+  UBaseType_t iPriority = 0;
+  TaskHandle_t pxTask;
+
+  xTaskCreate(myTask1, "myTask1", 1024, NULL, 1, &pxTask);
+  xTaskCreate(myTask2, "myTask2", 1024, NULL, 2, NULL);
+
+  vTaskPrioritySet(pxTask, 3);
+
+  iPriority = uxTaskPriorityGet(pxTask);
+  printf("iPriority = %d\n", iPriority);
 }
